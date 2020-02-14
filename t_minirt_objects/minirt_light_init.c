ac083998fe69cd901a6b7f5e765ac56ffdd9aff4
david <dadamouyal42@gmail.com>
2020-02-14 15:54:54 +0100

    restart

--- t_minirt_objects/minirt_light_init.c
+++
@@ -1,9 +0,0 @@
-#include "minirt_light_init.h"
-#include "t_minirt_object.h"
-#include <stdlib.h>
-
-int	minirt_light_init(t_minirt_object *obj, void *info_struct)
-{
-	obj->model = malloc(sizeof(t_simple_light_model));
-	obj->get_shadow = simple_light_model_shadow; 
-}
